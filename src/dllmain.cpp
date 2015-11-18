861d8131f882e82f4e9adf7d3ac587cd2db0dae4
Johan Sköld <johan@skold.cc>
2015-11-17 23:29:02 -0800

    Formatting

--- src/dllmain.cpp
+++ src/dllmain.cpp
@@ -3,8 +3,8 @@
 
 static HANDLE s_handle;
 
-void WriteToLog(const char str[], ...)
-{
+
+void WriteToLog (const char str[], ...) {
     char buffer[0x200];
     va_list args;
     va_start(args, str);
@@ -14,29 +14,27 @@ void WriteToLog(const char str[], ...)
     WriteFile(s_handle, buffer, len, &len, nullptr);
 }
 
-BOOL APIENTRY DllMain
-(
-    HMODULE hModule,
-    DWORD  ul_reason_for_call,
-    LPVOID lpReserved
-)
-{
+
+BOOL APIENTRY DllMain (HMODULE hModule,
+                       DWORD  ul_reason_for_call,
+                       LPVOID lpReserved) {
     REF(hModule, lpReserved);
 
     switch (ul_reason_for_call)
     {
-    case DLL_PROCESS_ATTACH:
-        s_handle = CreateFileA("test.log", GENERIC_WRITE, 0, nullptr, CREATE_ALWAYS, FILE_ATTRIBUTE_NORMAL, nullptr);
-        break;
+        case DLL_PROCESS_ATTACH:
+            s_handle = CreateFileA("test.log", GENERIC_WRITE, 0, nullptr, CREATE_ALWAYS, FILE_ATTRIBUTE_NORMAL, nullptr);
+            break;
 
-    case DLL_PROCESS_DETACH:
-        CloseHandle(s_handle);
-        break;
+        case DLL_PROCESS_DETACH:
+            CloseHandle(s_handle);
+            break;
 
-    case DLL_THREAD_ATTACH:
-    case DLL_THREAD_DETACH:
-        break;
+        case DLL_THREAD_ATTACH:
+        case DLL_THREAD_DETACH:
+            break;
     }
+
     return TRUE;
 }
 
