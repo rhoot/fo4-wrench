e3211f16cdea6d15e66d194748062c7efb650a46
Johan Sköld <johan@skold.cc>
2015-11-18 20:55:11 -0800

    Sorta working, but inlining may be a problem...

--- src/dllmain.cpp
+++ src/dllmain.cpp
@@ -20,15 +20,15 @@ static const IMAGE_EXPORT_DIRECTORY* GetExports (const uint8_t* imageBase) {
     assert(dosHeader->e_magic == IMAGE_DOS_SIGNATURE);
 
     auto ntHeaders = (const IMAGE_NT_HEADERS*)(imageBase + dosHeader->e_lfanew);
-    assert(header->Signature == IMAGE_NT_SIGNATURE);
-    assert(header->OptionalHeader.NumberOfRvaAndSizes > 0);
+    assert(ntHeaders->Signature == IMAGE_NT_SIGNATURE);
+    assert(ntHeaders->OptionalHeader.NumberOfRvaAndSizes > 0);
 
     return (const IMAGE_EXPORT_DIRECTORY*)(imageBase +
         ntHeaders->OptionalHeader.DataDirectory[IMAGE_DIRECTORY_ENTRY_EXPORT].VirtualAddress);
 }
 
 
-static const void* FindGetXScale () {
+static void* FindExport (const char exportName[]) {
     auto imageBase = (const uint8_t*)GetModuleHandleA(nullptr);
     assert(imageBase != nullptr);
 
@@ -37,18 +37,14 @@ static const void* FindGetXScale () {
     auto ordinals = (const uint16_t*)(imageBase + exports->AddressOfNameOrdinals);
     auto functions = (const uint32_t*)(imageBase + exports->AddressOfFunctions);
 
-    for (DWORD i = 0; i < exports->NumberOfFunctions; i++) {
-        WriteToLog("%08x\n", functions[i]);
-    }
-
-    for (DWORD i = 0; i < exports->NumberOfNames; i++) {
+    for (DWORD i = 0; i < exports->NumberOfNames; ++i) {
         auto name = (const char *)(imageBase + names[i]);
 
         auto result = CompareStringA(LOCALE_INVARIANT,
                                      0,
                                      name,
                                      -1,
-                                     "?GetXScale@?$Matrix2x4@M@Render@Scaleform@@QEBAMXZ",
+                                     exportName,
                                      -1);
 
         if (result == CSTR_EQUAL) {
@@ -61,16 +57,51 @@ static const void* FindGetXScale () {
     return nullptr;
 }
 
+template <class T>
+static void InstallHook() {
+    auto & hook = T::Get();
+    auto * name = T::GetExportName();
+    auto * src = FindExport(name);
 
-static void ApplyHook () {
-    auto getXScale = FindGetXScale();
-    if (!getXScale) {
-        WriteToLog("Unable to find GetXScale\n");
+    if (!src) {
+        WriteToLog("Could not find export %s, hook failed.\n", name);
         return;
     }
 
-    WriteToLog("Found GetXScale at %p\n", getXScale);
+    hook.SetupHook(src, &T::Hook);
+    hook.Hook();
 }
+
+template <class T>
+struct Hook {
+    static PLH::X64Detour& Get () {
+        static PLH::X64Detour s_detour;
+        return s_detour;
+    }
+
+    template <typename Fn>
+    static Fn GetOriginal (Fn) {
+        auto & hook = Get();
+        union { void* v; Fn f; } o;
+        o.v = hook.GetOriginal<void*>();
+        return o.f;
+    }
+};
+
+#define EXPORT_NAME(s)    static const char * GetExportName() { return s; }
+
+struct GetXScaleHook : Hook<GetXScaleHook> {
+    EXPORT_NAME("?GetXScale@?$Matrix2x4@M@Render@Scaleform@@QEBAMXZ");
+
+    float m_f;
+
+    float Hook () const {
+        auto original = GetOriginal(&GetXScaleHook::Hook);
+        auto f = (this->*original)();
+        printf_s("orig: %f\n", f);
+        return 5.0f;
+    }
+};
 
 
 BOOL APIENTRY DllMain (HMODULE hModule,
@@ -82,7 +113,7 @@ BOOL APIENTRY DllMain (HMODULE hModule,
     {
         case DLL_PROCESS_ATTACH:
             s_handle = CreateFileA("test.log", GENERIC_WRITE, 0, nullptr, CREATE_ALWAYS, FILE_ATTRIBUTE_NORMAL, nullptr);
-            ApplyHook();
+            InstallHook<GetXScaleHook>();
             break;
 
         case DLL_PROCESS_DETACH:
